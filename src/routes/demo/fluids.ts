327bd14e3cc8e27252a22b706d22b097d2779cb7
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-02-02 00:26:49 +0200

    +update shader

--- src/routes/demo/fluids.ts
+++ src/routes/demo/fluids.ts
@@ -1,8 +1,23 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
+import { LuminosityShader } from 'three/addons/shaders/LuminosityShader.js';
+import { BloomPass } from 'three/addons/postprocessing/BloomPass.js';
+import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
 import vertexShader from './vertexShader.glsl';
 import fragmentShader from './fragmentShader.glsl';
 import { GUI } from 'lil-gui';
+
+const options = {
+	Octaves: 4,
+	Color: '#ffffff',
+	['Lines count']: 100,
+	strength: 0.001,
+	radius: 0.001,
+	threshold: 0.5
+};
 
 class FluidsScene {
 	private scene: THREE.Scene = new THREE.Scene();
@@ -17,6 +32,9 @@ class FluidsScene {
 	private geometry: THREE.PlaneGeometry | null = null;
 	private controls: OrbitControls | null = null;
 	private gui: GUI | null = null;
+	private composer: EffectComposer | null = null;
+	private renderPass: RenderPass | null = null;
+	private bloomPass: BloomPass | null = null;
 
 	constructor(el: HTMLCanvasElement) {
 		this.camera.position.z = 1;
@@ -24,11 +42,13 @@ class FluidsScene {
 			canvas: el
 		});
 
+		// this.renderer.toneMapping = THREE.ReinhardToneMapping;
 		this.renderer.setClearColor('#000000');
 		this.renderer.setSize(window.innerWidth, window.innerHeight);
 		// this.controls = new OrbitControls(this.camera, this.renderer.domElement);
 
 		this.init();
+		// this.addPostProcessing();
 		this.addControls();
 		this.animate();
 
@@ -36,14 +56,18 @@ class FluidsScene {
 		window.addEventListener('resize', this.onResize.bind(this));
 	}
 
+	public addPostProcessing() {
+		if (!this.renderer) return;
+		this.composer = new EffectComposer(this.renderer);
+		this.renderPass = new RenderPass(this.scene, this.camera);
+
+		const outputPass = new OutputPass();
+		this.composer.addPass(outputPass);
+	}
+
 	public addControls() {
 		this.gui = new GUI();
 		if (!this.material) return;
-		const options = {
-			Octaves: this.material.uniforms.u_octaves.value,
-			Color: this.material.uniforms.u_color.value,
-			['Lines count']: this.material.uniforms.u_count.value
-		};
 
 		this.gui.add(options, 'Octaves', 0, 500).onChange(() => {
 			if (!this.material) return;
@@ -62,14 +86,32 @@ class FluidsScene {
 			this.material.uniforms.u_count.value = options['Lines count'];
 			this.material.needsUpdate = true;
 		});
+
+		if (this.bloomPass) {
+			this.gui.add(options, 'strength', 0, 3, 0.01).onChange(() => {
+				if (!this.bloomPass) return;
+				this.bloomPass.strength = options.strength;
+			});
+			this.gui.add(options, 'radius', 0, 1, 0.01).onChange(() => {
+				if (!this.bloomPass) return;
+				this.bloomPass.radius = options.radius;
+			});
+			this.gui.add(options, 'threshold', 0, 1, 0.01).onChange(() => {
+				if (!this.bloomPass) return;
+				this.bloomPass.threshold = options.threshold;
+			});
+		}
 	}
 
 	public init() {
-		// create responsive geometry plane
 		this.geometry = new THREE.PlaneGeometry(2, 2, 1, 1);
 
 		this.material = new THREE.ShaderMaterial({
 			side: THREE.DoubleSide,
+			blendDst: THREE.OneMinusSrcAlphaFactor,
+			transparent: true,
+			depthWrite: false,
+			blending: THREE.AdditiveBlending,
 			uniforms: {
 				u_time: { value: 0 },
 				u_resolution: { value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1) },
@@ -113,7 +155,8 @@ class FluidsScene {
 		if (this.material) {
 			this.material.uniforms.u_time.value += 0.006;
 		}
-		if (this.renderer) this.renderer.render(this.scene, this.camera);
+		if (this.composer) this.composer.render();
+		else if (this.renderer) this.renderer.render(this.scene, this.camera);
 	}
 }
 
--- src/routes/demo/fragmentShader.glsl
+++ src/routes/demo/fragmentShader.glsl
@@ -14,31 +14,60 @@ varying vec2 vUv;
 varying vec3 vPosition;
 float PI = 3.141592653589793238;
 
+float sdTorus( vec3 p, vec2 t )
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
+
+float sdTriPrism( vec3 p, vec2 h )
+{
+  vec3 q = abs(p);
+  return max(q.z-h.y,max(q.x*0.866025+p.y*0.5,-p.y)-h.x*0.5);
+}
+
+float sdSphere( vec3 p, float s )
+{
+  return length(p)-s;
+}
+
+// rotate torus
+vec3 rotate(vec3 p, float a) {
+    float c = cos(a);
+    float s = sin(a);
+    mat2 m = mat2(c, -s, s, c);
+    return vec3(m * p.xy, p.z);
+}
+
+vec2 rotate2d(vec2 _st, float _angle){
+    _st -= 0.5;
+    _st =  mat2(cos(_angle),-sin(_angle),
+                sin(_angle),cos(_angle)) * _st;
+    _st += 0.5;
+    return _st;
+}
 
 
 void main() {
       // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = vUv - vec2(0.5);
-    // uv.x *= u_resolution.x/u_resolution.y;
     uv *= 6.;
+
     vec3 col;
     float count = u_count;
     float Factor = mod(u_time,u_octaves);
     for(float i = 0. ;i< count;i++){
          float r = 2.;
-         float theta = (2. * 3.14)/count;
-         theta *= i;
+         float theta = (4. * 3.14)/count;
+         theta *= i * 0.5;
          vec2 a = r * vec2(cos(theta),sin(theta));
 
-         vec2 b = r * vec2(cos(theta * Factor + 3.14),sin(theta * Factor + 3.14));
-        //  col += .001/length(uv  -a);
-        //  col += .001/length(uv  -b);
+         vec2 b = r * vec2(cos(theta * Factor + PI),sin(theta * Factor + PI));
          float m = (a.y - b.y)/(a.x - b.x);
      
-        float d = .0002/abs(uv.y - a.y - m*(uv.x - a.x));
+        float d =.001/abs(uv.y - a.y  - m*(uv.x - a.x));
         if(length(uv) < r) {
             col += d;
-            // col += .0002/length(uv  -a);
         }
     }
      
