5a4a738920e159c74b41d420abd205234fd95d71
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-02-10 22:18:03 +0200

    +fix build 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,5 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-	plugins: [sveltekit(), glsl()],
-
-	build: {
-		rollupOptions: {
-			external: ['$app/environment']
-		}
-	}
+	plugins: [sveltekit(), glsl()]
 });
