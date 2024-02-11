bc090a1cef4768fb60d8716565301fab26143cd2
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-02-11 14:58:08 +0200

    +remove dev mode in sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,14 +2,11 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esnext" />
 /// <reference lib="webworker" />
-import { dev } from '$app/environment';
 
 const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
 
 if ('serviceWorker' in navigator) {
 	addEventListener('load', function () {
-		navigator.serviceWorker.register('./workers/sw.js', {
-			type: dev ? 'module' : 'classic'
-		});
+		navigator.serviceWorker.register('./workers/sw.js', {});
 	});
 }
