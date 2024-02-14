4abe5d9b20baa4a977498c5e75d8c668d4eb3e73
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-02-15 00:51:58 +0200

    +fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
-import resolve from 'rollup-plugin-node-resolve';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		glsl(),
-		resolve({
-			dedupe: ['svelte', 'svelte/transition', 'svelte/internal'] // important!
-		})
-	]
+	plugins: [sveltekit(), glsl()]
 });
