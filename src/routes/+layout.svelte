7a02c85eb407ac8ab0c75fbd773fbc4b651c990f
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-02-18 12:29:58 +0200

    +commented ws logic

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 	import io from 'socket.io-client';
 	import { onlineUsers } from '$lib/onlineUsers';
 
-	const socket = io('https://travelblogcms.onrender.com:3000');
+	// const socket = io('https://travelblogcms.onrender.com:3000');
 	// const socket = io('http://localhost:3000');
 
 	let transition: HTMLDivElement;
@@ -26,77 +26,77 @@
 	let openPopup = false;
 	let popupMessage = '';
 
-	popupStore.subscribe((value) => {
-		openPopup = value.show;
-		popupMessage = value.message;
-	});
+	// popupStore.subscribe((value) => {
+	// 	openPopup = value.show;
+	// 	popupMessage = value.message;
+	// });
 
-	afterNavigate(({ from }) => {
-		previousPage = from?.url?.pathname || previousPage;
-		currentPage = window.location.pathname;
-	});
+	// afterNavigate(({ from }) => {
+	// 	previousPage = from?.url?.pathname || previousPage;
+	// 	currentPage = window.location.pathname;
+	// });
 
-	pageTransition.subscribe((value) => {
-		if (value.start && transition && !animationStarted) {
-			animationStarted = true;
-			const tween = transitionIn({ toPage: value.toPage });
+	// pageTransition.subscribe((value) => {
+	// 	if (value.start && transition && !animationStarted) {
+	// 		animationStarted = true;
+	// 		const tween = transitionIn({ toPage: value.toPage });
 
-			if (tween) {
-				tween.then(() => {
-					transitionOut().then(() => {
-						animationStarted = false;
-					});
-				});
-			} else {
-				animationStarted = false;
-			}
-		}
-	});
+	// 		if (tween) {
+	// 			tween.then(() => {
+	// 				transitionOut().then(() => {
+	// 					animationStarted = false;
+	// 				});
+	// 			});
+	// 		} else {
+	// 			animationStarted = false;
+	// 		}
+	// 	}
+	// });
 
-	onMount(() => {
-		gsap.set(transition, { xPercent: -99.5, filter: 'blur(3px)' });
-		const uN = localStorage.getItem('userName');
-		if (uN) {
-			userName = uN;
-			socket.emit('join', { name: userName });
-		} else {
-			popupStore.update((state) => ({
-				...state,
-				show: true,
-				message: 'Please enter your name',
-				type: 'input'
-			}));
-		}
+	// onMount(() => {
+	// 	gsap.set(transition, { xPercent: -99.5, filter: 'blur(3px)' });
+	// 	const uN = localStorage.getItem('userName');
+	// 	if (uN) {
+	// 		userName = uN;
+	// 		socket.emit('join', { name: userName });
+	// 	} else {
+	// 		popupStore.update((state) => ({
+	// 			...state,
+	// 			show: true,
+	// 			message: 'Please enter your name',
+	// 			type: 'input'
+	// 		}));
+	// 	}
 
-		socket.on('users', (data) => {
-			if (socket.id && socket.id in data) delete data[socket.id];
-			onlineUsers.update((state) => ({
-				...state,
-				users: data
-			}));
-		});
+	// 	socket.on('users', (data) => {
+	// 		if (socket.id && socket.id in data) delete data[socket.id];
+	// 		onlineUsers.update((state) => ({
+	// 			...state,
+	// 			users: data
+	// 		}));
+	// 	});
 
-		const handleMouseMove = (e: MouseEvent) => {
-			const xPercent = Math.round((e.clientX / window.innerWidth) * 100);
-			const yPercent = Math.round((e.clientY / window.innerHeight) * 100);
-			socket.emit('mousemove', {
-				x: xPercent,
-				y: yPercent
-			});
-		};
+	// 	const handleMouseMove = (e: MouseEvent) => {
+	// 		const xPercent = Math.round((e.clientX / window.innerWidth) * 100);
+	// 		const yPercent = Math.round((e.clientY / window.innerHeight) * 100);
+	// 		socket.emit('mousemove', {
+	// 			x: xPercent,
+	// 			y: yPercent
+	// 		});
+	// 	};
 
-		document.addEventListener('mousemove', handleMouseMove);
+	// 	document.addEventListener('mousemove', handleMouseMove);
 
-		return () => {
-			socket.emit('leave', { name: userName });
-			socket.disconnect();
-		};
-	});
+	// 	return () => {
+	// 		socket.emit('leave', { name: userName });
+	// 		socket.disconnect();
+	// 	};
+	// });
 </script>
 
 <Loading />
 
-{#if openPopup}
+<!-- {#if openPopup}
 	<div
 		class="absolute
 		top-0 left-0 w-full h-full flex justify-center items-center z-[100]"
@@ -122,7 +122,7 @@
 			</div>
 		</Modal>
 	</div>
-{/if}
+{/if} -->
 
 <slot />
 
