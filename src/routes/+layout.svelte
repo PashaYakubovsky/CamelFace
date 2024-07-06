07790d35d8247408f6f15262a8422738ed3e0d1d
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-07-06 16:37:06 +0300

    +put analytic id to env

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	import { onMount } from 'svelte';
 	import { firebaseAuth } from '$lib/firebase/firebase.app';
 	import { v4 as uuidv4 } from 'uuid';
-
+	import { PUBLIC_GOOGLE_ANALYTICS_ID as GOOGLE_ANALYTICS_ID } from '$env/static/public';
 	let userId: string = localStorage.getItem('userId') || uuidv4();
 
 	onMount(() => {
@@ -24,6 +24,19 @@
 			}
 		});
 		firebaseAuth.onAuthStateChanged(async (user) => {});
+	});
+
+	onMount(() => {
+		//@ts-ignore
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			//@ts-ignore
+			dataLayer.push(arguments);
+		}
+		//@ts-ignore
+		gtag('js', new Date());
+		//@ts-ignore
+		gtag('config', GOOGLE_ANALYTICS_ID);
 	});
 </script>
 
