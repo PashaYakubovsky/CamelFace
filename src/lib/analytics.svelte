939c6356a1b12eed56dd6b1c729d441e8454a3fd
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-07-06 17:23:45 +0300

    +move analytics into sep component +clean up

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-WNGXJE3D6K', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WNGXJE3D6K">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-WNGXJE3D6K');
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,7 @@
 	import { onMount } from 'svelte';
 	import { firebaseAuth } from '$lib/firebase/firebase.app';
 	import { v4 as uuidv4 } from 'uuid';
+	import Analytics from '$lib/analytics.svelte';
 
 	let userId: string = localStorage.getItem('userId') || uuidv4();
 
@@ -26,33 +27,11 @@
 		});
 		firebaseAuth.onAuthStateChanged(async (user) => {});
 	});
-
-	$: {
-		if (typeof gtag !== 'undefined') {
-			gtag('config', 'G-WNGXJE3D6K', {
-				page_title: document.title,
-				page_path: $page.url.pathname
-			});
-		}
-	}
 </script>
 
-<svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WNGXJE3D6K">
-	</script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-
-		gtag('js', new Date());
-		gtag('config', 'G-WNGXJE3D6K');
-	</script>
-</svelte:head>
-
 <slot />
+
+<Analytics />
 
 <Copyright />
 
