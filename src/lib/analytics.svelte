fed437e110370a37406045cea4bd76cf90ada964
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-09-26 23:00:54 +0300

    +update analytic

--- src/lib/analytics.svelte
+++ src/lib/analytics.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from "$app/stores"
 	// get is production or development
-	import { onMount } from 'svelte';
 
 	$: {
-		const mode = import.meta.env.MODE;
-		if (typeof gtag !== 'undefined' && mode !== 'development') {
-			console.log('gtag is defined');
-			gtag('config', 'G-WNGXJE3D6K', {
+		const mode = import.meta.env.MODE
+		if (typeof gtag !== "undefined" && mode !== "development") {
+			console.log("gtag is defined")
+			gtag("config", "G-PR3VD9Z8NB", {
 				page_title: document.title,
-				page_path: $page.url.pathname
-			});
+				page_path: $page.url.pathname,
+			})
 		}
 	}
 </script>
 
 <svelte:head>
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-WNGXJE3D6K">
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-PR3VD9Z8NB">
 	</script>
 	<script>
-		window.dataLayer = window.dataLayer || [];
+		window.dataLayer = window.dataLayer || []
 
 		function gtag() {
-			dataLayer.push(arguments);
+			dataLayer.push(arguments)
 		}
 
-		gtag('js', new Date());
-		gtag('config', 'G-WNGXJE3D6K');
+		gtag("js", new Date())
+		gtag("config", "G-PR3VD9Z8NB")
 	</script>
 </svelte:head>
