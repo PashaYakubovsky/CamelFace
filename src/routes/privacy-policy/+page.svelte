087361c3d508b9f44517429f7d589175e5b9947f
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-11-15 00:07:52 +0200

    +add privacy policy page

---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,69 @@
+<div class="container">
+	Privacy Policy Last updated: [{new Date(
+		"2024-10-10T00:00:00Z",
+	).toLocaleDateString()}]
+
+	<h1>Privacy Policy</h1>
+	<p>
+		Your privacy is important to us. It is our policy to respect your privacy
+		regarding any information we may collect from you across our application.
+	</p>
+
+	<h2>1. Information We Collect</h2>
+	<p>
+		We only collect information about you if we have a reason to do so – for
+		example, to provide our services, to communicate with you, or to make our
+		services better.
+	</p>
+
+	<h2>2. How We Use Information</h2>
+	<p>
+		We use the information we collect in various ways, including to provide,
+		operate, and maintain our app, improve, personalize, and expand our app,
+		understand and analyze how you use our app, develop new products, services,
+		features, and functionality, communicate with you, either directly or
+		through one of our partners, including for customer service, to provide you
+		with updates and other information relating to the app, and for marketing
+		and promotional purposes.
+	</p>
+
+	<h2>3. Sharing Information</h2>
+	<p>
+		We do not share your personal information with anyone except to comply with
+		the law, develop our products, or protect our rights.
+	</p>
+
+	<h2>4. Security</h2>
+	<p>
+		We take the security of your personal information seriously and take
+		reasonable measures to protect it. However, remember that no method of
+		transmission over the internet, or method of electronic storage is 100%
+		secure and reliable, and we cannot guarantee its absolute security.
+	</p>
+
+	<h2>5. Changes to This Privacy Policy</h2>
+	<p>
+		We may update our Privacy Policy from time to time. We will notify you of
+		any changes by posting the new Privacy Policy on this page. You are advised
+		to review this Privacy Policy periodically for any changes. Changes to this
+		Privacy Policy are effective when they are posted on this page.
+	</p>
+</div>
+
+<style>
+	.container {
+		max-width: 800px;
+		margin: 0 auto;
+		padding: 20px;
+	}
+
+	h1 {
+		text-align: center;
+		margin-bottom: 20px;
+	}
+
+	p {
+		margin-bottom: 10px;
+		line-height: 1.6;
+	}
+</style>
